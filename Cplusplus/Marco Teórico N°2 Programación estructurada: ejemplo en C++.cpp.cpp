aabfc7394636bbdbd2cf468166809d813dd554cf
KahoriDiazUCSM <131702350+KahoriDiazUCSM@users.noreply.github.com>
2024-03-22 17:47:42 -0500

    Marco Teórico N°2 Programación estructurada: ejemplo en C++.cpp.cpp
    Programación estructurada: ejemplo en C++

---
+++ Cplusplus/Marco Teórico N°2 Programación estructurada: ejemplo en C++.cpp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 15; 
+
+    if (x > 10) {
+        cout << "x es mayor que 10" << endl;
+    } else {
+        cout << "x es menor o igual que 10" << endl;
+    }
+
+    return 0;
+}
