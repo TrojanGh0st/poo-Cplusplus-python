e993aff4fc51727d7ad7bcadc9012cf6417eb32e
KahoriDiazUCSM <131702350+KahoriDiazUCSM@users.noreply.github.com>
2024-03-22 22:10:39 -0500

    Update Marco Teórico N°3 Paradigma P.POO C++.cpp

--- Cplusplus/Marco Teórico N°3 Paradigma P.POO C++.cpp
+++ Cplusplus/Marco Teórico N°3 Paradigma P.POO C++.cpp
@@ -1,39 +1,51 @@
 /*Programación orientada a objetos:
 Se enfoca en el uso de objetos y clases para encapsular datos y comportamientos.
 Proporciona un alto nivel de reutilización de código a través de la herencia y la composición.
-Ejemplo en Java:*/
+Ejemplo en Java: -> C++*/
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-
 class Persona {
 public:
     string nombre;
     int edad;
-};
+    
+    Persona(string nombre = "", int edad = 0) : nombre(nombre), edad(edad) {}
 
+    void saludar() {
+        cout << "Hola, soy " << nombre << " y tengo " << edad << " años." << endl;
+    }
+};
 
 class Estudiante : public Persona {
 public:
     string carrera;
+
+    Estudiante(string nombre = "", int edad = 0, string carrera = "") : Persona(nombre, edad), carrera(carrera) {}
+
+    void estudiar() {
+        cout << nombre << " está estudiando " << carrera << "." << endl;
+    }
 };
 
 int main() {
-    Estudiante estudiante1;
-    estudiante1.nombre = "Juan";
-    estudiante1.edad = 20;
-    estudiante1.carrera = "Ingeniería";
+    Persona P01;
+    P01.nombre = "Pablo";
+    P01.edad = 10;
+    cout << "Nombre: " << P01.nombre << endl;
+    cout << "Edad: " << P01.edad << endl;
+    P01.saludar();
 
-    cout << "Nombre: " << estudiante1.nombre << ", Edad: " << estudiante1.edad << ", Carrera: " << estudiante1.carrera << endl;
-
-    Estudiante estudiante2;
-    estudiante2.nombre = "Maria";
-    estudiante2.edad = 22;
-    estudiante2.carrera = "Medicina";
-
-    cout << "Nombre: " << estudiante2.nombre << ", Edad: " << estudiante2.edad << ", Carrera: " << estudiante2.carrera << endl;
+    Estudiante E01;
+    E01.nombre = "Esteban";
+    E01.edad = 20;
+    E01.carrera = "Ingeniería";
+    cout << "Nombre: " << E01.nombre << endl;
+    cout << "Edad: " << E01.edad << endl;
+    E01.saludar();
+    E01.estudiar();
 
     return 0;
 }
