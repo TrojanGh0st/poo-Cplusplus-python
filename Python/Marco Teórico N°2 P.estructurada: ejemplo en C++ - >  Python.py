92a14835840f981fdcc58fd71128fd14efb10b35
KahoriDiazUCSM <131702350+KahoriDiazUCSM@users.noreply.github.com>
2024-03-22 20:45:38 -0500

    Update Marco Teórico N°2 P.estructurada: ejemplo en C++ - >  Python.py

--- Python/Marco Teórico N°2 P.estructurada: ejemplo en C++ - >  Python.py
+++ Python/Marco Teórico N°2 P.estructurada: ejemplo en C++ - >  Python.py
@@ -1,3 +1,6 @@
+#Programación estructurada: enfatiza la organización lógica y clara del código, y utiliza estructuras
+#de control de flujo como el if-then-else, while, for, entre otros. Un ejemplo en C++ -> Python sería:
+
 x = 15
 
 if x > 10:
