7515c72431355c4a87964c2bbe5365ac97f5ec0a
KahoriDiazUCSM <131702350+KahoriDiazUCSM@users.noreply.github.com>
2024-03-22 20:51:58 -0500

    Update and rename p4.py to Marco Teórico N°3 P.Estructurada Python.py

---
+++ Python/Marco Teórico N°3 P.Estructurada Python.py
@@ -0,0 +1,18 @@
+#PARADIGMA
+#Programación estructurada:
+#Se enfoca en la organización lógica y clara del código.
+#Utiliza estructuras de control de flujo, como el if-then-else, while, for, entre otros, para controlar el
+#flujo de ejecución.
+#Ejemplo en C:   -> Python
+
+def main():
+    a = 5
+    b = 3
+
+    if a > b:
+        print("a es mayor que b")
+    else:
+        print("b es mayor o igual que a")
+
+if __name__ == "__main__":
+    main()
