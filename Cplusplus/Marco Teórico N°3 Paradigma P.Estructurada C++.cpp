5f91c96b5e0bfbfbf9e98d93bbe1c5c0e409b33e
KahoriDiazUCSM <131702350+KahoriDiazUCSM@users.noreply.github.com>
2024-03-22 20:43:28 -0500

    Update and rename c4.cpp to Marco Teórico N°3 Paradigma P.Estructurada C++.cpp

---
+++ Cplusplus/Marco Teórico N°3 Paradigma P.Estructurada C++.cpp
@@ -0,0 +1,22 @@
+/* PARADIGMA
+Programación estructurada:
+Se enfoca en la organización lógica y clara del código.
+Utiliza estructuras de control de flujo, como el if-then-else, while, for, entre otros, para controlar el
+flujo de ejecución.
+Ejemplo en C:   -> C++*/ 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 3;
+
+    if (a > b) {
+        cout << "a es mayor que b" << endl;
+    } else {
+        cout << "b es mayor o igual que a" << endl;
+    }
+
+    return 0;
+}
